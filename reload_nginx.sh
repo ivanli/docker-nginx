3e77d50a59c038a36395b2e195d0f3c172d48fa1
Ivan Li <ivankyli@gmail.com>
2020-04-07 18:03:27 +1200

    Kill nginx process if reload fails

--- reload_nginx.sh
+++ reload_nginx.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-while inotifywait -t 30 -r -e create -e modify -e delete /etc/letsencrypt /conf /etc/docker/environment
+echo "Waiting for quiet period..."
+while inotifywait -t 60 -r -e create -e modify -e delete /etc/letsencrypt /conf /etc/docker/environment
 do
   echo "... more changes detected, waiting again..."
 done
+echo "... no more file changes detected."
 
+echo "Reloading nginx..."
 /docker-entrypoint.sh /usr/sbin/nginx -s reload
+if [ $? -ne 0 ]; then
+  echo "... failed to reload nginx, killing nginx to force restart"
+  kill -SIGQUIT $(cat /var/run/nginx.pid || echo 1)
+  exit 1
+fi
+
+echo "... nginx reloaded successfully."
